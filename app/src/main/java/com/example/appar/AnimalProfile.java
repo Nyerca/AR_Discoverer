7367c59efb635880df034e8a8e99c3d947f238eb
enrico.facciani <enrico.facciani@studio.unibo.it>
2020-07-09 15:01:08 +0200

    Add homepage, collection and animal info

---
+++ app/src/main/java/com/example/appar/AnimalProfile.java
@@ -0,0 +1,16 @@
+package com.example.appar;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AnimalProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.animal_profile);
+
+    }
+
+}
